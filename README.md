b8528da08385a85fa57ce8f79a67832ffe67c3f6
Qupol Two Times <ilya.v-i-n-o@yandex.ru>
2023-02-28 13:22:44 +0300

    finish step 3

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/QupolTwoTimes/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/QupolTwoTimes/frontend-project-46/actions)
+
+# Hello, Dear Users :)
+## Welcome to my new project ;)
+* ### About the project
+
+* #### Video with worc proccess
+    [![start ](https://asciinema.org/a/563490.svg)](https://asciinema.org/a/563490)
+
+   
--- src/index.js
+++ src/index.js
@@ -1,34 +1,12 @@
 import { readFileSync } from 'fs';
-import _ from 'lodash';
 import { resolve } from 'path';
 import { cwd } from 'process';
+import genDiff from './getDiff.js';
 
-const genDiff = (data1, data2) => {
-  const keys1 = Object.keys(data1);
-  const keys2 = Object.keys(data2);
-  const keys = _.union(keys1, keys2).sort();
-
-  const getStr = keys.map((key) => {
-    if (_.has(data1, key) && _.has(data2, key)) {
-      if (data1[key] === data2[key]) {
-        return `  ${key}: ${data1[key]}`;
-      }
-      return [`- ${key}: ${data1[key]}`, `+ ${key}: ${data2[key]}`];
-    } if (_.has(data1, key) && !(_.has(data2, key))) {
-      return `- ${key}: ${data1[key]}`;
-    } return `+ ${key}: ${data2[key]}`;
-  });
-
-  const tmp = getStr.flat();
-  console.log(`{\n  ${tmp.join('\n  ')}\n}`);
-  return `{\n  ${tmp.join('\n  ')}\n}`;
-};
+const getFileContent = (pathOfFile) => readFileSync(resolve(cwd(), pathOfFile), 'utf-8');
 
 export default (filepath1, filepath2) => {
-  const data1 = readFileSync(resolve(cwd(), filepath1), 'utf-8');
-  const data2 = readFileSync(resolve(cwd(), filepath2), 'utf-8');
-
-  const firstObj = JSON.parse(data1);
-  const secondObj = JSON.parse(data2);
-  genDiff(firstObj, secondObj);
+  const firstObj = JSON.parse(getFileContent(filepath1));
+  const secondObj = JSON.parse(getFileContent(filepath2));
+  return genDiff(firstObj, secondObj);
 };
